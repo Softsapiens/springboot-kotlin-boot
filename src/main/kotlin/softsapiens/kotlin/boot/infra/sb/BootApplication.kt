f20a70b77ba6fcec640ec9fab53a1087482077e0
Dani <daniel.pousmontardit@telefonica.com>
2022-10-25 18:28:25 +0200

    initial working kick-off

---
+++ src/main/kotlin/softsapiens/kotlin/boot/infra/sb/BootApplication.kt
@@ -0,0 +1,44 @@
+package softsapiens.kotlin.boot.infra.sb
+
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.annotation.Id
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+
+@SpringBootApplication
+class BootApplication {
+    @Bean
+    fun runner(repository: CustomerRepository) = ApplicationRunner {
+        runBlocking {
+            val customers = flowOf("John", "El", "Foo")
+                .map { Customer(null, it) }
+            repository.saveAll(customers)
+                .collect{ println(it) }
+        }
+    }
+
+    @Bean
+    fun http(repository: CustomerRepository) = coRouter {
+        GET("/customers") {
+            ServerResponse.ok().bodyAndAwait(repository.findAll())
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<BootApplication>(*args)
+}
+
+interface CustomerRepository : CoroutineCrudRepository<Customer, Int>
+
+data class Customer(@Id val id: Int?, val name: String)
+
---
+++ src/test/kotlin/softsapiens/kotlin/boot/infra/sb/BootApplicationTests.kt
@@ -0,0 +1,13 @@
+package softsapiens.kotlin.boot.infra.sb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
